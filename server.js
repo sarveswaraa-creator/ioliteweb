a236db8f9658f1c732438be36ce4702c0c14c614
Sarvesdeveloper <sarves744@gmail.com>
2020-07-26 15:58:12 +0530

    file dir

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var appPath = '../iolite';
+var appPath = '/iolite';
 
 app.use('/',express.static(path.join(__dirname,appPath)))
-.get('*',(req,res)=> res.sendFile(path.join(__dirname,appPath +'/iolite/index.html')))
+.get('*',(req,res)=> res.sendFile(path.join(__dirname,appPath +'/index.html')))
 .listen(PORT,()=>console.log('klklkl ${PORT}'));
