7d1f86f2c36135542085a3a5fed74d9c1952f3d7
Sarvesdeveloper <sarves744@gmail.com>
2020-07-26 15:52:41 +0530

    path changes

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000;
 var appPath = '../iolite';
 
 app.use('/',express.static(path.join(__dirname,appPath)))
-.get('*',(req,res)=> res.sendFile(path.join(__dirname,appPath +'../index.html')))
+.get('*',(req,res)=> res.sendFile(path.join(__dirname,appPath +'/iolite/index.html')))
 .listen(PORT,()=>console.log('klklkl ${PORT}'));
