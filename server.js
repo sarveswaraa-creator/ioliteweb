feeb75e2ec1e7f2b922795085ab93ab583d90f36
Sarvesdeveloper <sarves744@gmail.com>
2020-07-26 16:49:26 +0530

    new angular build

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 4000;
-var appPath = '/iolite';
+var appPath = '/src';
 
 app.use('/',express.static(path.join(__dirname,appPath)))
 .get('*',(req,res)=> res.sendFile(path.join(__dirname,appPath +'/index.html')))
